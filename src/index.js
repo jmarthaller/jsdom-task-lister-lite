b5711729058bae3b98e2c8c717bc66e7c91b3a81
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-13 14:24:18 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+
+function renderOneInput(inputObject) {
+  
+}
+
+
+
+  const submitButton = document.querySelector("#create-task-form")
+
+  submitButton.addEventListener("submit", function(event) {
+    event.preventDefault()
+    //debugger
+    const inputText = document.querySelector("input[type='text']").value
+    //event.target //.newTaskDescription   //.new-task-description.value matches name but does not post
+    //const newTaskDescription = document.getElementById("new-task-description");
+
+    // inputText.innerText = newTaskDescription.value
+    //inputText.value 
+
+    // renderOneInput(inputText)
+
+    const lists = document.querySelector('#tasks')
+
+    const innerLi = document.createElement('li')
+  //debugger
+    innerLi.textContent = inputText
+                      
+    lists.append(innerLi)
+   
+    event.target.reset()
+    //debugger
+  })
+  
 });
