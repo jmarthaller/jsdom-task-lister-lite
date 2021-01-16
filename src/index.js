bd24cb708dac9b1292238736c331a54a4b85df6d
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-16 09:38:07 -0500

    clean

--- src/index.js
+++ src/index.js
@@ -1,36 +1,77 @@
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
   
 
-function renderOneInput(inputObject) {
+// function renderOneInput(inputObject) {
   
-}
+// }
 
+//   const submitButton = document.querySelector("#create-task-form")
 
+//   submitButton.addEventListener("submit", function(event) {
+//     event.preventDefault()
+//     //debugger
+//     const inputText = document.querySelector("input[type='text']").value
+//     //event.target //.newTaskDescription   //.new-task-description.value matches name but does not post
+//     //const newTaskDescription = document.getElementById("new-task-description");
 
-  const submitButton = document.querySelector("#create-task-form")
+//     // inputText.innerText = newTaskDescription.value
+//     //inputText.value 
 
-  submitButton.addEventListener("submit", function(event) {
-    event.preventDefault()
-    //debugger
-    const inputText = document.querySelector("input[type='text']").value
-    //event.target //.newTaskDescription   //.new-task-description.value matches name but does not post
-    //const newTaskDescription = document.getElementById("new-task-description");
+//     // renderOneInput(inputText)
 
-    // inputText.innerText = newTaskDescription.value
-    //inputText.value 
+//     const lists = document.querySelector('#tasks')
 
-    // renderOneInput(inputText)
-
-    const lists = document.querySelector('#tasks')
-
-    const innerLi = document.createElement('li')
-  //debugger
-    innerLi.textContent = inputText
+//     const innerLi = document.createElement('li')
+//   //debugger
+//     innerLi.textContent = inputText
                       
-    lists.append(innerLi)
+//     lists.append(innerLi)
    
-    event.target.reset()
-    //debugger
-  })
+//     event.target.reset()
+//     //debugger
+//   })
   
-});
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
