e0213a33438dec2dd1d8659ac974bc656e05b07c
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-16 20:19:37 -0500

    redo

--- src/index.js
+++ src/index.js
@@ -75,3 +75,43 @@
 
 
 
+
+
+{/* <form id="form">
+  <label>Test field: <input type="text"></label>
+  <br><br>
+  <button type="submit">Submit form</button>
+</form>
+<p id="log"></p> */}
+
+// function logSubmit(event) {
+//     log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+//     event.preventDefault();
+//   }
+  
+//   const form = document.getElementById('form');
+//   const log = document.getElementById('log');
+//   form.addEventListener('submit', logSubmit);
+
+
+const taskForm = document.querySelector('#create-task-form')
+const taskList = document.querySelector('#tasks')
+
+
+taskForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    let newTask = document.createElement('li')
+    newTask.classList.add('newest')
+    //debugger
+    // console.log(e.target)
+    // const taskData = e.target.value
+    const newTaskDescription = document.getElementById("new-task-description");
+    newTask.textContent = newTaskDescription.value
+
+    taskList.append(newTask)
+
+    e.target.reset()
+})
+
+
